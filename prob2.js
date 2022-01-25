2b771ea26a69ed8829e50a7936ddc78b0c3f6bbc
LucetTin5 <lucettin5@gmail.com>
2022-01-25 12:17:29 +0900

    lottery

--- prob2.js
+++ prob2.js
@@ -3,4 +3,26 @@ function solution(lottos, win_nums) {
     lottos = lottos.sort((a, b) => a - b);
     win_nums = win_nums.sort((a, b) => a - b);
     
+    let matches = 0;
+    lottos.forEach((num) => {
+        for (let i = 0; i < 6; i++) {
+            if (num === win_nums[i]) {
+                matches += 1;
+                break;
+            }
+        }
+    })
+    let rank = 7;
+    if (matches >= 2) {
+        rank -= matches;
+    } else { rank = 6 }
+    const zeros = lottos.filter((num) => num === 0);
+    const zeros_length = zeros.length;
+    if (zeros_length === 6) {
+        return [1, 6];
+    } else if (zeros_length === 0) {
+        return [rank, rank];   
+    } else {
+        return [rank - zeros_length, rank];
+    }
 }

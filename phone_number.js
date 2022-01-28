3fda49d3478d084441b94e0d738b05c3376a4036
LucetTin5 <lucettin5@gmail.com>
2022-01-28 13:19:37 +0900

    phone_number

--- phone_number.js
+++ phone_number.js
@@ -1,5 +1,4 @@
 function solution(phone_numeber) {
-  const length = phone_numeber.length;
-  const last_four = phone_numeber.slice(length - 4);
-  return "".concat("*".repeat(length - 5), last_four);
+  const re = new RegExp(/\d(?=\d{4})/, "g");
+  return phone_numeber.replace(re, "*");
 }

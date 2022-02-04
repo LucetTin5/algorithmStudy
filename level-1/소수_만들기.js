c4294d58422be50618de6ee194985717cf42dbee
LucetTin5 <lucettin5@gmail.com>
2022-02-04 15:44:29 +0900

    세 수를 더해 소수가 되는 경우

--- level-1/소수_만들기.js
+++ level-1/소수_만들기.js
@@ -16,12 +16,24 @@ function isPrime(x) {
     return 0;
   // 5의 배수
   else if (x % 10 === 5 || x % 10 === 0) return 0;
-  // 그 외 -> 2의 배수가 제외되었으니 3부터 2씩 증가
   else {
-    for (let n = 3; n < Math.ceil(Math.sqrt(x)); n += 2) {
-      if (x % n === 0) return 0;
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+      if (x % i === 0) return 0;
     }
   }
   return 1;
 }
-function solution(nums) {}
+function solution(nums) {
+  let arr = [];
+  const length = nums.length;
+  for (let i = 0; i < length - 2; i++) {
+    for (let j = i + 1; j < length - 1; j++) {
+      for (let k = j + 1; k < length; k++) {
+        arr.push(nums[i] + nums[j] + nums[k]);
+      }
+    }
+  }
+  let answer = 0;
+  arr.map((x) => (answer += isPrime(x)));
+  return answer;
+}

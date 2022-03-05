910c59447a3e6a753c563a9b0fbb6309fa363134
LucetTin5 <lucettin5@gmail.com>
2022-03-05 13:56:15 +0900

    N개의 최소공배수

--- level-2/N개의_최소공배수.js
+++ level-2/N개의_최소공배수.js
@@ -1,6 +1,20 @@
+function GCD(a, b) {
+  const big = a > b ? a : b;
+  const small = a < b ? a : b;
+  if (big % small === 0) return small;
+  else {
+    return GCD(small, big % small);
+  }
+}
+function LCM(a, b) {
+  return Math.round((a * b) / GCD(a, b));
+}
+
 function solution(arr) {
-  let answer = arr.reduce((a, b) => a * b, 1);
-  let temp = arr.sort((a, b) => a - b);
-  const min = Math.min(...temp);
-  return min;
+  do {
+    let [a, b] = arr.slice(0, 2);
+    arr = arr.splice(2);
+    arr.push(LCM(a, b));
+  } while (arr.length > 2);
+  return LCM(arr[0], arr[1]);
 }
